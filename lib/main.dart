9e8a0a47604b982077572e6020d89542dfd2042f
stashkiv <andr.stashkiv@gmail.com>
2020-01-03 14:15:40 +0200

    Fix code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -19,23 +23,228 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Login Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: new StreamBuilder(
-        stream: auth.onAuthStateChanged,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return HomePage();
-          }
-          return LoginRegister();
-        },
-      ),
-      routes: <String, WidgetBuilder>{
-        '/home': (BuildContext context) => new HomePage(),
-        '/login': (BuildContext context) => new LoginRegister()
+        title: 'Flutter Login Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: InitUser());
+  }
+}
+
+class InitUser extends StatefulWidget {
+  @override
+  _InitUserState createState() => _InitUserState();
+}
+
+class _InitUserState extends State<InitUser> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  StreamSubscription iosSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    register();
+  }
+
+  @override
+  void dispose() {
+    if (iosSubscription != null) iosSubscription.cancel();
+    super.dispose();
+  }
+
+  void register() {
+    if (Platform.isIOS) {
+      iosSubscription =
+          _firebaseMessaging.onIosSettingsRegistered.listen((data) {});
+
+      _firebaseMessaging
+          .requestNotificationPermissions(IosNotificationSettings());
+    }
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        _showItemDialog(message);
       },
+      onBackgroundMessage: Platform.isIOS ? null : myBackgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        _navigateToItemDetail(message);
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {});
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+    });
+    _subscribeToTopic();
+  }
+
+  void iOS_Permission() {
+    _firebaseMessaging.requestNotificationPermissions(
+        IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {});
+  }
+
+  _subscribeToTopic() async {
+    _firebaseMessaging.subscribeToTopic('general');
+  }
+
+  static Future<dynamic> myBackgroundMessageHandler(
+      Map<String, dynamic> message) async {
+    if (message.containsKey('data')) {
+      // Handle data message
+      final dynamic data = message['data'];
+      return data;
+    }
+
+    if (message.containsKey('notification')) {
+      // Handle notification message
+      final dynamic notification = message['notification'];
+      return notification;
+    }
+    return message['data'];
+    // Or do other work.
+  }
+
+  Widget _buildDialog(BuildContext context, Item item) {
+    return AlertDialog(
+      title: Text(item.itemId),
+      content: Text("${item.itemMessage}"),
+      actions: <Widget>[
+        FlatButton(
+          child: const Text('CLOSE'),
+          onPressed: () {
+            Navigator.pop(context, false);
+          },
+        ),
+        FlatButton(
+          child: const Text('SHOW'),
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+      ],
+    );
+  }
+
+  void _showItemDialog(Map<String, dynamic> message) {
+    showDialog<bool>(
+      context: context,
+      builder: (_) => _buildDialog(context, _itemForMessage(message)),
+    ).then((bool shouldNavigate) {
+      if (shouldNavigate == true) {
+        _navigateToItemDetail(message);
+      }
+    });
+  }
+
+  void _navigateToItemDetail(Map<String, dynamic> message) {
+    final Item item = _itemForMessage(message);
+    Navigator.popUntil(context, (Route<dynamic> route) => route is PageRoute);
+    if (!item.route.isCurrent) {
+      Navigator.push(context, item.route);
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: auth.onAuthStateChanged,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        }
+        return LoginRegister();
+      },
+    );
+  }
+}
+
+final Map<String, Item> _items = <String, Item>{};
+
+Item _itemForMessage(Map<String, dynamic> message) {
+  final dynamic data = message['data'] ?? message;
+  final String itemId = data["id"] == null ? "" : data["id"];
+  final String itemMessage = data["message"] == null ? "" : data["message"];
+  final Item item = _items.putIfAbsent(
+      itemId, () => Item(itemId: itemId, itemMessage: itemMessage));
+  return item;
+}
+
+class Item {
+  Item({this.itemId, this.itemMessage});
+
+  final String itemId;
+  final String itemMessage;
+
+  StreamController<Item> _controller = StreamController<Item>.broadcast();
+
+  Stream<Item> get onChanged => _controller.stream;
+
+  String _status;
+
+  String get status => _status;
+
+  set status(String value) {
+    _status = value;
+    _controller.add(this);
+  }
+
+  static final Map<String, Route<void>> routes = <String, Route<void>>{};
+
+  Route<void> get route {
+    final String routeName = '/detail/$itemId';
+    return routes.putIfAbsent(
+      routeName,
+      () => MaterialPageRoute<void>(
+        settings: RouteSettings(name: routeName),
+        builder: (BuildContext context) => DetailPage(itemId, itemMessage),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatefulWidget {
+  DetailPage(this.itemId, this.itemMessage);
+
+  final String itemId;
+  final String itemMessage;
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Item _item;
+  StreamSubscription<Item> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _item = _items[widget.itemId];
+    _subscription = _item.onChanged.listen((Item item) {
+      if (!mounted) {
+        _subscription.cancel();
+      } else {
+        setState(() {
+          _item = item;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${_item.itemId}"),
+      ),
+      body: Material(
+        child: Center(child: Text("${_item.itemMessage}")),
+      ),
     );
   }
 }
